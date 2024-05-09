6e9c5948168d74cae7986a123c6f785534b1760c
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-05-09 14:08:08 +0100

    create separate settings modules; ignore F405 rule in settings modules

--- polarrouteserver/asgi.py
+++ polarrouteserver/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polarrouteserver.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polarrouteserver.base")
 
 application = get_asgi_application()
--- polarrouteserver/celery.py
+++ polarrouteserver/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polarrouteserver.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polarrouteserver.settings.base")
 
 app = Celery(
     "polarrouteserver", broker="amqp://localhost:5672", backend="rpc://localhost:5672"
