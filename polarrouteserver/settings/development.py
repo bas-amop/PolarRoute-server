b58a5f23c939fd9616a3d02a0523d866bdc6e27a
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-08-07 15:50:45 +0100

    add yaml configs

--- polarrouteserver/settings/development.py
+++ polarrouteserver/settings/development.py
@@ -1,1 +1,9 @@
+import yaml
+from pathlib import Path
+
 from .base import *
+
+with open(Path("config", "development.yaml"), "r") as f:
+    config = yaml.load(f, Loader=yaml.Loader)
+
+MESH_PATH = config.get("mesh_path")
--- polarrouteserver/settings/staging.py
+++ polarrouteserver/settings/staging.py
@@ -1,1 +1,9 @@
+from pathlib import Path
+import yaml
+
 from .base import *
+
+with open(Path("config", "development.yaml"), "r") as f:
+    config = yaml.load(f, Loader=yaml.Loader)
+
+MESH_PATH = config.get("mesh_path")
