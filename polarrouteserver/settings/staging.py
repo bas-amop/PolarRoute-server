09bac07920f936b2d93076bf7460bc19758b2b58
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-09-09 10:58:23 +0100

    alter config for staging server

--- polarrouteserver/settings/staging.py
+++ polarrouteserver/settings/staging.py
@@ -3,7 +3,13 @@ import yaml
 
 from .base import *
 
-with open(Path("config", "development.yaml"), "r") as f:
+# DEBUG = False
+
+with open(Path("config", "staging.yaml"), "r") as f:
     config = yaml.load(f, Loader=yaml.Loader)
 
 MESH_PATH = config.get("mesh_path")
+
+ALLOWED_HOSTS.extend(config.get("allowed_hosts"))
+
+CELERY_BROKER_URL=config.get("celery_broker_url")
