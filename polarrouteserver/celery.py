405b849ee409faee6d52d13ddde93a615086b195
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-09-10 17:25:09 +0100

    import settings

--- polarrouteserver/celery.py
+++ polarrouteserver/celery.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 
 from celery import Celery, signals
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polarrouteserver.settings.production")
@@ -27,38 +28,31 @@ def debug_task(self):
 @signals.setup_logging.connect
 def on_celery_setup_logging(**kwargs):
     config = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s%(process)d/%(thread)d%(name)s%(funcName)s %(lineno)s%(levelname)s%(message)s',
-                'datefmt': "%Y/%m/%d %H:%M:%S"
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s%(process)d/%(thread)d%(name)s%(funcName)s %(lineno)s%(levelname)s%(message)s",
+                "datefmt": "%Y/%m/%d %H:%M:%S",
             }
         },
-        'handlers': {
-            'celery': {
-                'level': 'INFO',
-                'class': 'logging.FileHandler',
-                'filename': Path(settings.BASE_DIR, "logs", "celery.log"),
-                'formatter': 'default'
+        "handlers": {
+            "celery": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "filename": Path(settings.BASE_DIR, "logs", "celery.log"),
+                "formatter": "default",
             },
-            'default': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
-                'formatter': 'default'
-            }
-        },
-        'loggers': {
-            'celery': {
-                'handlers': ['celery'],
-                'level': 'INFO',
-                'propagate': False
+            "default": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "default",
             },
         },
-        'root': {
-            'handlers': ['default'],
-            'level': 'DEBUG'
+        "loggers": {
+            "celery": {"handlers": ["celery"], "level": "INFO", "propagate": False},
         },
+        "root": {"handlers": ["default"], "level": "DEBUG"},
     }
 
     logging.config.dictConfig(config)
