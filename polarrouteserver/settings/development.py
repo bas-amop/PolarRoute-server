aa05fcaf8dfa0bf0fbead8df3bf054be27b7d219
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-09-23 21:18:06 +0100

    add cors settings

--- polarrouteserver/settings/development.py
+++ polarrouteserver/settings/development.py
@@ -7,3 +7,14 @@ with open(Path("config", "development.yaml"), "r") as f:
     config = yaml.load(f, Loader=yaml.Loader)
 
 MESH_PATH = config.get("mesh_path")
+
+# Use django-cors-headers package in development
+INSTALLED_APPS.append("corsheaders")
+MIDDLEWARE += [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:9000",
+]
+CORS_ALLOW_METHODS = ("DELETE", "GET", "POST", "OPTIONS")
