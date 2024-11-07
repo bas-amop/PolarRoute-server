af764e3169e6a048a7f448cc417eac4938cbefe1
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-11-07 16:39:02 +0000

    register models so they show in admin panel

--- route_api/admin.py
+++ route_api/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Route, Mesh, Job
+
+admin.site.register(Route)
+admin.site.register(Mesh)
+admin.site.register(Job)
