9d4be7c1867dc4875ad5e17f1cdf75e33faf86ef
davidwilby <24752124+davidwilby@users.noreply.github.com>
2024-12-22 10:26:48 +0000

    if smoothed routes unavailable, return unsmoothed in place

--- polarrouteserver/route_api/serializers.py
+++ polarrouteserver/route_api/serializers.py
@@ -18,3 +18,43 @@ class RouteSerializer(serializers.ModelSerializer):
             "polar_route_version",
             "info",
         ]
+
+    def to_representation(self, instance):
+        """Returns unsmoothed routes if available when smoothed routes have failed."""
+        data = super().to_representation(instance)
+
+        smoothed = {}
+        unsmoothed = {}
+        data["json"] = []
+        for key in ("traveltime", "fuel"):
+            smoothed[key] = (
+                [
+                    x
+                    for x in data["json"]
+                    if x[0]["features"][0]["properties"]["objective_function"] == key
+                ]
+                if data["json"] is not None
+                else []
+            )
+            unsmoothed[key] = (
+                [
+                    x
+                    for x in data["json_unsmoothed"]
+                    if x[0]["features"][0]["properties"]["objective_function"] == key
+                ]
+                if data["json_unsmoothed"] is not None
+                else []
+            )
+
+            # if there is no smoothed route available, use unsmoothed in its place
+            if len(smoothed[key]) == 0 and len(unsmoothed[key]) == 1:
+                data["json"].extend(unsmoothed[key])
+                data["info"] = {
+                    "error": f"Smoothing failed for {key}-optimisation, returning unsmoothed route."
+                }
+            elif len(smoothed[key]) == 0 and len(unsmoothed[key]) == 0:
+                data["info"] = {"error": f"No routes available for {key}-optimisation."}
+            else:
+                data["json"].extend(unsmoothed[key])
+
+        return data
