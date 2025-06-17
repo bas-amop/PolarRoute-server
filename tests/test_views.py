275dc5b383dd77b5b0d3490efd3ff8784e357ce1
thomaszwagerman <thozwa@bas.ac.uk>
2025-06-17 13:33:42 +0100

    add test vessel config, settings and test for creating updating routes

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,9 +8,45 @@ from rest_framework.test import APIRequestFactory
 import pytest
 
 from polarrouteserver import __version__ as polarrouteserver_version
-from polarrouteserver.route_api.views import EvaluateRouteView, MeshView, RouteView, RecentRoutesView
+from polarrouteserver.route_api.views import EvaluateRouteView, MeshView, VehicleView, RouteView, RecentRoutesView
 from polarrouteserver.route_api.models import Job, Route
 from .utils import add_test_mesh_to_db
+
+
+class TestVehicleRequest(TestCase):
+    with open(settings.TEST_VEHICLE_PATH) as fp:
+        vessel_config = json.load(fp)
+
+    data = dict(vessel_config)
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    # Test vehicle is created successfully
+    def test_create_update_vehicle(self, data=data):
+        request = self.factory.post("/api/vehicle/", data=data, format="json")
+
+        response = VehicleView.as_view()(request)
+
+        self.assertEqual(response.status_code, 202)
+
+        # Test creating a duplicate vehicle fails
+        request = self.factory.post("/api/vehicle/", data=data, format="json")
+        response = VehicleView.as_view()(request)
+
+        self.assertEqual(response.status_code, 406)
+
+        # Test force_properties allows for existing vessel_type to be updated
+        data.update(
+            {
+                "force_properties":True
+            }
+        )
+
+        request = self.factory.post("/api/vehicle/", data=data, format="json")
+        response = VehicleView.as_view()(request)
+
+        self.assertEqual(response.status_code, 202)
 
 
 class TestRouteRequest(TestCase):
