c62fcf76831d9e4db317f79a931cc419b6d77754
davidwilby <24752124+davidwilby@users.noreply.github.com>
2025-06-26 21:26:53 +0100

    reduce duplicaation

--- polarrouteserver/frontend.py
+++ polarrouteserver/frontend.py
@@ -17,19 +17,26 @@ default_sic_date = datetime.date.today() - datetime.timedelta(days=1)
 
 app = DjangoDash('PolarRoute', add_bootstrap_links=True)
 
+def amsr_layer(date: datetime.date):
+    return dl.TileLayer(
+            id="amsr",
+            url="https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AMSRU2_Sea_Ice_Concentration_12km/default/"+date.strftime("%Y-%m-%d")+"/GoogleMapsCompatible_Level6/{z}/{y}/{x}",
+            attribution="Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href=\"https://earthdata.nasa.gov\">ESDIS</a>) with funding provided by NASA/HQ.",
+            maxZoom=6,
+            )
+
+def server_url(request):
+    "Return server url from request object in dpd callback."
+    return f"{request.scheme}://{request.get_host()}"
+
+
 app.layout = html.Div(
     [
         dbc.Spinner(color="primary"),
         dl.Map([
            dl.TileLayer(id="basemap", attribution=("© OpenStreetMap contributors"), zIndex=0,),
            dl.FullScreenControl(),
-           dl.LayersControl([dl.Overlay(
-                dl.TileLayer(
-                id="amsr",
-                url="https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AMSRU2_Sea_Ice_Concentration_12km/default/"+default_sic_date.strftime("%Y-%m-%d")+"/GoogleMapsCompatible_Level6/{z}/{y}/{x}",
-                attribution="Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href=\"https://earthdata.nasa.gov\">ESDIS</a>) with funding provided by NASA/HQ.",
-                maxZoom=6,
-                ), name="AMSR", checked=False, id="amsr-overlay")
+           dl.LayersControl([dl.Overlay(amsr_layer(default_sic_date), name="AMSR", checked=False, id="amsr-overlay")
            ], id="layers-control"),
         ], center=[-72, -67], zoom=4, style={"height": "80vh"}, id="map"),
         dcc.Slider(min=-30, max=0, step=1, value=0, id='amsr-date-slider', marks=None, tooltip={"placement": "top", "always_visible": False}),
@@ -42,18 +49,6 @@ app.layout = html.Div(
         Input("amsr-date-slider", "value"),
         prevent_initial_call=True)
 def update_amsr_overlay(slider_value):
-    sic_date = (default_sic_date - datetime.timedelta(days=abs(slider_value))).strftime("%Y-%m-%d")
-
-    return dl.TileLayer(
-            id="amsr",
-            url="https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AMSRU2_Sea_Ice_Concentration_12km/default/"+sic_date+"/GoogleMapsCompatible_Level6/{z}/{y}/{x}",
-            attribution="Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href=\"https://earthdata.nasa.gov\">ESDIS</a>) with funding provided by NASA/HQ.",
-            maxZoom=6,
-            )
-
-
-def server_url(request):
-    "Return server url from request object in dpd callback."
-    return f"{request.scheme}://{request.get_host()}"
-
+    sic_date = (default_sic_date - datetime.timedelta(days=abs(slider_value)))
+    return amsr_layer(sic_date)
 
